4d34e02f22fbb794a6f2ec02737ab4aefb9fcaca
Idan <idan.avin@gmail.com>
2021-06-19 02:13:54 +0300

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CostumersComponent } from './costumers/costumers.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'costumers', component: CostumersComponent },
+  { path: '**', component: NotFoundComponent },  // Wildcard route for a 404 page
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    DashboardComponent,
+    CostumersComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/costumers/costumers.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CostumersComponent } from './costumers.component';
+
+describe('CostumersComponent', () => {
+  let component: CostumersComponent;
+  let fixture: ComponentFixture<CostumersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CostumersComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CostumersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
