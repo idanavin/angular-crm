b1ccfc908d94aa698c5d34795cbdddb023baf85f
idan <idan.avin@gmail.com>
2021-07-08 15:44:59 +0300

    fixed paginator translations

--- src/app/shared/paginator/paginator.component.ts
+++ src/app/shared/paginator/paginator.component.ts
@@ -7,6 +7,14 @@ import { TranslateService } from '@ngx-translate/core';
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.scss'],
+  providers: [
+    {
+      provide: MatPaginatorIntl,
+      deps: [TranslateService],
+      useFactory: (translateService: TranslateService) =>
+        new PaginatorComponent(translateService).getPaginatorIntl(),
+    },
+  ],
 })
 export class PaginatorComponent implements OnInit {
   @Input() length?: number;
@@ -37,14 +45,25 @@ export class PaginatorComponent implements OnInit {
     return paginatorIntl;
   }
 
-  private getRangeLabel(page: number, pageSize: number, length: number): string {
+  private getRangeLabel(
+    page: number,
+    pageSize: number,
+    length: number
+  ): string {
     if (length === 0 || pageSize === 0) {
-        return this.translate.instant('RANGE_PAGE_LABEL_1', { length });
+      return this.translate.instant('RANGE_PAGE_LABEL_1', { length });
     }
     length = Math.max(length, 0);
     const startIndex = page * pageSize;
     // If the start index exceeds the list length, do not try and fix the end index to the end.
-    const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
-    return this.translate.instant('RANGE_PAGE_LABEL_2', { startIndex: startIndex + 1, endIndex, length });
-}
+    const endIndex =
+      startIndex < length
+        ? Math.min(startIndex + pageSize, length)
+        : startIndex + pageSize;
+    return this.translate.instant('RANGE_PAGE_LABEL_2', {
+      startIndex: startIndex + 1,
+      endIndex,
+      length,
+    });
+  }
 }
--- src/app/shared/paginator/paginator.module.ts
+++ src/app/shared/paginator/paginator.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginatorComponent } from './paginator.component';
-import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
-import { TranslateService } from '@ngx-translate/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { LanguageModule } from 'src/app/language/language.module';
 
 
@@ -15,12 +14,6 @@ import { LanguageModule } from 'src/app/language/language.module';
     CommonModule,
     MatPaginatorModule,
     LanguageModule
-  ],
-  providers: [
-    {
-      provide: MatPaginatorIntl, deps: [TranslateService],
-      useFactory: (translateService: TranslateService) => new PaginatorComponent(translateService).getPaginatorIntl()
-    }
   ],
   exports: [
     PaginatorComponent
