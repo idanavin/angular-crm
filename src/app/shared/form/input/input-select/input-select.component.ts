c2da1a29f9b0750cefe197e11ce62d3649cde2bf
Idan <idan.avin@gmail.com>
2021-08-13 05:19:10 +0300

    Changes to translation files Added backgroud to inputs with select or autocomplete

--- src/app/shared/form/input/input-select/input-select.component.ts
+++ src/app/shared/form/input/input-select/input-select.component.ts
@@ -1,12 +1,15 @@
 import { Component, forwardRef, Input } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
-import { MatFormFieldAppearance, MatFormFieldDefaultOptions } from '@angular/material/form-field';
+import {
+  MatFormFieldAppearance,
+  MatFormFieldDefaultOptions,
+} from '@angular/material/form-field';
 import { InputComponent } from '../input.component';
 
 @Component({
   selector: 'app-input-select',
   templateUrl: './input-select.component.html',
-  styleUrls: ['../input.component.scss'],
+  styleUrls: ['../input.component.scss', './input-select.component.scss'],
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
@@ -17,7 +20,7 @@ import { InputComponent } from '../input.component';
 })
 // * Does not include input but extend ControlValueAccessor implemented on InputComponent
 export class InputSelectComponent extends InputComponent {
-  @Input() appearance: MatFormFieldAppearance = "outline";
+  @Input() appearance: MatFormFieldAppearance = 'outline';
 
   @Input()
   public select: string[] | undefined;
