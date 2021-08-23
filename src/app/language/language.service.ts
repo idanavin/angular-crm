62d5fd6cc78b7505447d48a32405c2eccce18b6e
Idan <idan.avin@gmail.com>
2021-08-23 15:20:00 +0300

    Added variables for language support

--- src/app/language/language.service.ts
+++ src/app/language/language.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Inject } from "@angular/core";
 import { DOCUMENT } from "@angular/common";
-import { Subject } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -12,15 +10,27 @@ export class LanguageService {
   constructor(private translateService: TranslateService, @Inject(DOCUMENT) private document: Document) { }
 
   changeLanguage(language: string): void {
-    let htmlTag = this.document.getElementsByTagName("html")[0] as HTMLHtmlElement;
-    htmlTag.dir = language === "he" ? "rtl" : "ltr";
     this.translateService.setDefaultLang(language);
     this.translateService.use(language);
+    this.changeHtmlDirection(language);
+    this.changeClassForBody(language);
     localStorage.setItem('lang', language);
   }
 
   setDefaultLanguage(): void {
     const localLanguage = localStorage.getItem('lang')
     localLanguage ? this.changeLanguage(localLanguage) : this.changeLanguage('en')
+  }
+
+  changeHtmlDirection(language: string) {
+    const htmlTag = this.document.getElementsByTagName("html")[0] as HTMLHtmlElement;
+    htmlTag.dir = language === "he" ? "rtl" : "ltr";
+  }
+
+  changeClassForBody(language: string) {
+    const dir = language === "he" ? "rtl" : "ltr";
+    const bodyTag = this.document.getElementsByTagName("body")[0] as HTMLBodyElement;
+    bodyTag.classList.remove(...['ltr', 'rtl'])
+    bodyTag.classList.add(dir);
   }
 }
