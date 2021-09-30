7858d82a6841d8a884a28ff33599b20a86b05155
Idan <idan.avin@gmail.com>
2021-09-30 09:23:59 +0300

    Added mongoose to employee

--- server/src/employee/employee.module.ts
+++ server/src/employee/employee.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from 'src/users/users.module';
 import { EmployeeController } from './employee.controller';
+import { EmployeeSchema } from './employee.model';
 import { EmployeeService } from './employee.service';
 import { JwtAdminStrategy } from './jwt-admin.strategy';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, MongooseModule.forFeature([{name: 'Users', schema: EmployeeSchema}])],
   controllers: [EmployeeController],
   providers: [EmployeeService, JwtAdminStrategy]
 })
